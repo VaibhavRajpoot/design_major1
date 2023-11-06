f913a635fabc588a56425b315104e9733801eee8
VaibhavRajpoot <vaibhavrajpoot46gmail.com>
2023-11-06 20:40:41 +0530

    first commit

---
+++ build/Basic_Robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vaibhav/Desktop/robo/src/Basic_Robot_description
+# Build directory: /home/vaibhav/Desktop/robo/build/Basic_Robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/vaibhav/Desktop/robo/src
+# Build directory: /home/vaibhav/Desktop/robo/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("design")
